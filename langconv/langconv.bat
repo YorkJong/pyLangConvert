f71f2658967120d9477950eb06d7dd1d7e90096b
York <yukuan.jiang@gmail.com>
2012-12-11 16:45:40 +0800

    langconv: Refined langconv.bat

--- langconv/langconv.bat
+++ langconv/langconv.bat
@@ -4,31 +4,25 @@ set langconv=langconv.py
 set dic=dic.xls
 set tgt=lang_id.h
 echo =^> Generate a C header file (%tgt%) of language ID enumeration.
-if not exist %dic% echo File "%dic%" Not Found!
-if exist %dic% %langconv% lang_id -o%tgt% %dic%
+%langconv% lang_id -o%tgt% %dic%
 
 set dic=dic.xls
 set tgt=msg_id.h
 echo =^> Generate a C header file (%tgt%) of message ID enumeration.
-if not exist %dic% echo File "%dic%" Not Found!
-if exist %dic% %langconv% msg_id -o%tgt% %dic%
+%langconv% msg_id -o%tgt% %dic%
 
 set dic=dic.xls
 set lst=char.lst
 set tgt=verify.report
 echo =^> Generate a report file (%tgt%) that lists used-but-not-listed
 echo    chars and listed-but-not-used chars.
-if not exist %dic% echo File "%dic%" Not Found!
-if not exist %lst% echo File "%lst%" Not Found!
-if exist %dic% if exist %lst% %langconv% verify -o%tgt% %dic% %lst%
+%langconv% verify -o%tgt% %dic% %lst%
 
 set dic=dic.xls
 set lst=char.lst
 set tgt=mlang.i
 echo =^> Generate a C included file (%tgt%) listing an array that packs
 echo    multilanguage messages.
-if not exist %dic% echo File "%dic%" Not Found!
-if not exist %lst% echo File "%lst%" Not Found!
-if exist %dic% if exist %lst% %langconv% pack -o%tgt% %dic% %lst%
+%langconv% pack -o%tgt% %dic% %lst%
 
 pause
