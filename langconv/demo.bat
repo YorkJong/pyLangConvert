f63b279abeca38a2772590cb6d4ad4b5b5a46e3f
York <yukuan.jiang@gmail.com>
2012-12-12 19:15:25 +0800

    langconv: Split langconv.bat into demo.bat and test.bat and added help.bat

---
+++ langconv/demo.bat
@@ -0,0 +1,28 @@
+@echo off
+set langconv=langconv.exe
+
+set dic=dic.xls
+set tgt=lang_id.h
+echo =^> Generate a C header file (%tgt%) of language ID enumeration.
+%langconv% lang_id -o%tgt% %dic%
+
+set dic=dic.xls
+set tgt=msg_id.h
+echo =^> Generate a C header file (%tgt%) of message ID enumeration.
+%langconv% msg_id -o%tgt% %dic%
+
+set dic=dic.xls
+set lst=char.lst
+set tgt=verify.report
+echo =^> Generate a report file (%tgt%) that lists used-but-not-listed
+echo    chars and listed-but-not-used chars.
+%langconv% verify -o%tgt% %dic% %lst%
+
+set dic=dic.xls
+set lst=char.lst
+set tgt=mlang.i
+echo =^> Generate a C included file (%tgt%) listing an array that packs
+echo    multilanguage messages.
+%langconv% pack -o%tgt% %dic% %lst%
+
+pause
---
+++ langconv/help.bat
@@ -0,0 +1,15 @@
+langconv -h
+pause
+
+langconv lang_id -h
+pause
+
+langconv msg_id -h
+pause
+
+langconv verify -h
+pause
+
+langconv pack -h
+pause
+
